dbd1e9bdaadfc958546b0c3959a4214015ce08c9
Felix Ruiz de Arcaute <ruiz.felixruiz@gmail.com>
2018-10-12 08:29:11 +0200

    update component to use genreic model plugin

--- addon/components/editor-plugins/date-card.js
+++ addon/components/editor-plugins/date-card.js
@@ -1,6 +1,7 @@
 import { reads } from '@ember/object/computed';
 import Component from '@ember/component';
 import layout from '../../templates/components/editor-plugins/date-card';
+import InsertPrimitivePropertyCardMixin from '@lblod/ember-generic-model-plugin-utils/mixins/insert-primitive-property-card-mixin';
 
 /**
 * Card displaying a hint of the Date plugin
@@ -9,46 +10,16 @@ import layout from '../../templates/components/editor-plugins/date-card';
 * @class DateCard
 * @extends Ember.Component
 */
-export default Component.extend({
+export default Component.extend(InsertPrimitivePropertyCardMixin, {
   layout,
 
-  /**
-   * Region on which the card applies
-   * @property location
-   * @type [number,number]
-   * @private
-  */
-  location: reads('info.location'),
-
-  /**
-   * Unique identifier of the event in the hints registry
-   * @property hrId
-   * @type Object
-   * @private
-  */
-  hrId: reads('info.hrId'),
-
-  /**
-   * The RDFa editor instance
-   * @property editor
-   * @type RdfaEditor
-   * @private
-  */
-  editor: reads('info.editor'),
-
-  /**
-   * Hints registry storing the cards
-   * @property hintsRegistry
-   * @type HintsRegistry
-   * @private
-  */
-  hintsRegistry: reads('info.hintsRegistry'),
-
   actions: {
-    insert(){
-      let mappedLocation = this.get('hintsRegistry').updateLocationToCurrentIndex(this.get('hrId'), this.get('location'));
-      this.get('hintsRegistry').removeHintsAtLocation(this.get('location'), this.get('hrId'), 'editor-plugins/date-card');
-      this.get('editor').replaceTextWithHTML(...mappedLocation, this.get('info').value);
+    async insert(data){
+      this.insert(await data.rdfaProperty,
+                  data.plainValue,
+                  data.rdfaContent,
+                  'editor-plugins/date-card',
+                  [{ who: 'editor-plugins/date-card' }]);
     }
   }
 
