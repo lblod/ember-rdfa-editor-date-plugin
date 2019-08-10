74295a673b62fe23714c6ead3913454909f23464
Aad Versteden <madnificent@gmail.com>
2019-08-10 14:24:12 +0200

    Update card to new interface and native classes

--- addon/components/editor-plugins/date-card.js
+++ addon/components/editor-plugins/date-card.js
@@ -1,3 +1,4 @@
+import { action } from '@ember/object';
 import { computed } from '@ember/object';
 import Component from '@ember/component';
 import layout from '../../templates/components/editor-plugins/date-card';
@@ -12,51 +13,55 @@ import moment from 'moment';
 * @class DateCard
 * @extends Ember.Component
 */
-export default Component.extend(InsertPrimitivePropertyCardMixin, {
-  layout,
-  hintOwner: 'editor-plugins/date-card',
+export default class EditorPluginDateCardComponent extends Component.extend( InsertPrimitivePropertyCardMixin ) {
+  layout = layout;
+  hintOwner = 'editor-plugins/date-card';
+  minutes = 0; // Setting default for minutes makes input nicer
 
-  isDateTime: computed('info.datatype', function(){
-    return this.get('info.datatype') == 'http://www.w3.org/2001/XMLSchema#dateTime';
-  }),
+  @computed('info.rdfaProperty.range.rdfaType')
+  get isDateTime(){
+    return this.get('info.rdfaProperty.range.rdfaType') == 'http://www.w3.org/2001/XMLSchema#dateTime';
+  }
 
-  isValidInput: computed('isDateTime', 'minutes', 'hours', function() {
+  @computed('isDateTime', 'minutes', 'hours')
+  get isValidInput() {
   	const hours = parseInt(this.hours);
   	const minutes = parseInt(this.minutes);
     return !this.isDateTime || (hours >= 0 && hours < 24 && minutes >= 0 && minutes < 60);
-  }),
-
-  isInvalidInput: not('isValidInput'),
-
-  isDateTime: computed('info.rdfaProperty.range.rdfaType', function(){
-    return this.get('info.rdfaProperty.range.rdfaType') == 'http://www.w3.org/2001/XMLSchema#dateTime';
-  }),
+  }
+  @not('isValidInput') isInvalidInput;
 
   formatTimeStr(isoStr, hours){
     if(hours)
       return moment(isoStr).format('LL, LT');
     return moment(isoStr).format('LL');
-  },
+  }
 
-  insert(propertyMeta, value, content){
+  async insertData(propertyMeta, value, content){
     let mappedLocation = this.get('hintsRegistry').updateLocationToCurrentIndex(this.get('hrId'), this.get('location'));
     this.get('hintsRegistry').removeHintsAtLocation(this.get('location'), this.get('hrId'), this.hintOwner);
     this.get('editor').replaceTextWithHTML(...mappedLocation, this.getRdfa(propertyMeta, value, content), [{ who: 'editor-plugins/date-card' }]);
-  },
+  }
 
-  actions: {
-    async insert(data){
-      this.insert(await data.rdfaProperty,
-                  data.plainValue,
-                  data.rdfaContent);
-    },
+  @action
+  async insertDate(data){
+    this.insertData(await data.rdfaProperty,
+                data.plainValue,
+                data.rdfaContent);
+  }
 
-    async insertDateTime(data, hours, minutes){
+  @action
+  async insertDateTime(){
+    if( this.isValidInput ) {
+      const data = this.info;
+      const hours = this.hours;
+      const minutes = this.minutes;
+
       let dateTimeIso = moment(data.rdfaContent, data.rdfaContentDateFormat).hours(hours || 0).minutes(minutes || 0).toISOString();
       let value = this.formatTimeStr(dateTimeIso, hours);
 
-      this.insert(await data.rdfaProperty,
-            value, dateTimeIso);
+      this.insertData(await data.rdfaProperty,
+                  value, dateTimeIso);
     }
   }
-});
+}
